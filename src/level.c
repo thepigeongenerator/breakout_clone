748ac2463e8722e1144e9e5c10574c4856d0f607
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-09-04 16:16:08 +0200

    add parentheses

--- src/level.c
+++ src/level.c
@@ -51,7 +51,7 @@ void level_init(Level* level) {
 static void update_player(Bouncer* bouncer, Ball* ball, const Uint8* keys) {
     // if move bouncer LEFT
     if (keys[SDL_SCANCODE_A] || keys[SDL_SCANCODE_LEFT]) {
-        if (bouncer->pos.x < 0 == false) {
+        if ((bouncer->pos.x) < 0 == false) {
             bouncer->pos.x -= BOUNCER_SPEED;
 
             if (ball->moving == false)
@@ -61,7 +61,7 @@ static void update_player(Bouncer* bouncer, Ball* ball, const Uint8* keys) {
 
     // if move bouncer RIGHT
     if (keys[SDL_SCANCODE_D] || keys[SDL_SCANCODE_RIGHT]) {
-        if ((bouncer->pos.x + bouncer->width) > SCREEN_WIDTH == false) {
+        if (((bouncer->pos.x + bouncer->width) > SCREEN_WIDTH) == false) {
             bouncer->pos.x += BOUNCER_SPEED; // increase the bouncer pos
 
             if (ball->moving == false)
